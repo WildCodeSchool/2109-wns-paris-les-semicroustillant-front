80284c460fc6703cf2fd2af6cc5ba0820962b952
dev-talbi <dev.talbi@gmail.com>
2021-11-24 16:58:50 +0100

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { useQuery, gql } from '@apollo/client';
 import { getUsers } from './schemaTypes';
+import Navbar from './components/Navbar';
+import Table from './components/TableUser';
 
 import AllUsers from './components/AllUsers';
 import DeleteUser from './components/DeleteUser';
@@ -9,11 +11,10 @@ import DeleteUser from './components/DeleteUser';
 function App(): JSX.Element {
   return (
     <div>
-      Hello world
-      <div>
-        <AllUsers />
-        <DeleteUser />
-      </div>
+      <Navbar />
+      <Table />
+      <AllUsers />
+      <DeleteUser />
     </div>
   );
 }
