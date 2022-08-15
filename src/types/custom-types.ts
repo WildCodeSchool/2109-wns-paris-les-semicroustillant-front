82ce5fb6ab7401bac30a6a21a4d4f98359cf8bad
Fredy <fredy.ly.wcs@gmail.com>
2022-08-15 17:14:13 +0200

    update custom types

--- src/types/custom-types.ts
+++ src/types/custom-types.ts
@@ -7,3 +7,48 @@ export interface IDecodedToken {
   iat: number;
   exp: number;
 }
+
+// ====================================================
+// CUSTOM COMPONENTS
+// ====================================================
+export interface ComponentActionBtnProps {
+  children: React.ReactNode;
+  onClick: () => void;
+};
+
+export interface ComponentTitleBarProps {
+  title: string;
+  onClickRigthBtn: () => void;
+  displayRightBtn?: boolean;
+};
+
+export interface ComponentCustomFilterChechBoxProps {
+  label: string;
+  onClick: () => void;
+};
+
+
+// ====================================================
+// TASK CARD
+// ====================================================
+export interface ITicketCard {
+  _id: string;
+  created_by: string;
+  subject: string;
+  status: string | null;
+  deadline: Date;
+  description: string | null;
+  initial_time_estimated: number | null;
+  total_time_spent: number | null;
+  advancement: number | null;
+  project_id: string | null;
+  users: IUserTicket[] | null;
+}
+
+export interface IExistingTickets {
+  allTickets: ITicketCard[];
+}
+
+export interface IUserTicket {
+  _id: string;
+}
